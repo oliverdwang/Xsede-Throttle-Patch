1a8598a0d2654bfec71701b575755d84802e67a4
Oliver <43011252+oliverdwang@users.noreply.github.com>
2018-09-20 23:44:18 -0400

    Added dynamic reset triggers and native single run capability

--- Xsede-Throttle-Patch.ps1
+++ Xsede-Throttle-Patch.ps1
@@ -1,5 +1,6 @@
 #USER SETTINGS
-$xtu_path = 'C:\Program Files (x86)\Intel\Intel(R) Extreme Tuning Utility\Client\XTUCli.exe'
+$xtu_path = 'C:\Program Files (x86)\Intel\Intel(R) Extreme Tuning Utility\Client\XTUCli.exe' #Directory
+$run_once = $false #Boolean
 $static_bat_update_rate = 30 #Seconds
 $static_ac_update_rate = 5 #Seconds
 $undervolt = -80 #Millivolts
@@ -9,6 +10,14 @@ $bat_boost_time = 96 #Seconds, max 96
 $ac_long_boost = 15 #Watts
 $ac_short_boost = 25 #Watts
 $ac_boost_time = 96 #Seconds, max 96
+
+#ADVANCED SETTINGS
+#If the sum of all CPU utilization percentages is less than this value
+#AND the actual TDP is lower than what is defined above, reset values
+$aggregate_utilization_trigger = 100
+
+
+#END OF SETTINGS
 
 #Check if on battery
 $isBattery=$false
@@ -21,29 +30,52 @@ if ($status -ne "Running") { start-service -name "XTU3SERVICE"}
 #Undervolt on initial run
 & $xtu_path -t -id 34 -v $undervolt
 
-while($true)
-{
-	if(isBattery -eq $true)
-	{
+do {
+    #Get current TDP
+    $TDP = & $xtu_path -t -id 1000
+
+    #Get current CPU utilization
+    $util_core_0 = & $xtu_path -t -id 90
+    $util_core_1 = & $xtu_path -t -id 91
+    $util_core_2 = & $xtu_path -t -id 92
+    $util_core_3 = & $xtu_path -t -id 93
+    $util_sum = $util_core_0 + $util_core_1 + $util_core_2 + $util_core_3
+
+	if($isBattery -eq $true) {
 		# If running on battery
-		Start-Sleep $static_bat_update_rate
-		# Set long boost
-		& $xtu_path -t -id 48 -v $bat_long_boost
-		# Set short boost
-		& $xtu_path -t -id 47 -v $bat_short_boost
-		# Set boost time
-		& $xtu_path -t -id 66 -v $bat_boost_time
+        
+        #If the current TDP is less than or equal to the user set TDPs AND any core is at >aggregate_utilization_trigger%, reset values
+        if((($TDP -le $bat_long_boost) -or ($TDP -le $bat_short_boost)) -and ($util_sum -ge $aggregate_utilization_trigger)) {
+            #Set long boost
+		    & $xtu_path -t -id 48 -v $bat_long_boost
+		    #Set short boost
+		    & $xtu_path -t -id 47 -v $bat_short_boost
+		    #Set boost time
+		    & $xtu_path -t -id 66 -v $bat_boost_time
+        }
+        
+        #If user only wants to run once, skip delay
+        if($run_once -eq $false) {
+            Start-Sleep $static_bat_update_rate
+        }
 	} else {
 		#If running on AC
-		Start-Sleep $static_ac_update_rate
-		# Set long boost
-		& $xtu_path -t -id 48 -v $ac_long_boost
-		# Set short boost
-		& $xtu_path -t -id 47 -v $ac_short_boost
-		# Set boost time
-		& $xtu_path -t -id 66 -v $ac_boost_time
+
+        if((($TDP -le $ac_long_boost) -or ($TDP -le $ac_short_boost)) -and ($util_sum -ge $aggregate_utilization_trigger)) {
+		    #Set long boost
+		    & $xtu_path -t -id 48 -v $ac_long_boost
+	    	#Set short boost
+		    & $xtu_path -t -id 47 -v $ac_short_boost
+		    #Set boost time
+		    & $xtu_path -t -id 66 -v $ac_boost_time
+        }
+        
+        #If user only wants to run once, skip delay
+        if($run_once -eq $false) {
+            Start-Sleep $static_ac_update_rate
+        }
 	}
-	
-	#Apply settings
-	stop-process -id $PID -force
-}
+} while ($run_once -eq $false)
+
+#If user only wants to run once, program will exit loop and stop
+stop-process -id $PID -force
